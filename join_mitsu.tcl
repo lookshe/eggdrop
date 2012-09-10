3cb7dabc47c4b2faea44dfd7a4f1957f294cb999
lookshe <lookshe@fumuga.com>
2012-09-10 22:48:15 +0200

    problem binding to channel so check for channel in script

--- join_mitsu.tcl
+++ join_mitsu.tcl
@@ -1,8 +1,11 @@
-bind join - "#mitsu_chat_runde" join_help
+bind join - * join_help
 
 proc join_help {nick host hand chan} {
    global botnick
    if {$botnick == $nick} {
+      return 0
+   }
+   if {$chan != "#mitsu_chat_runde"} {
       return 0
    }
    putserv "PRIVMSG $chan :Willkommen im MFF-Chat $nick, stelle einfach deine Frage oder sag einfach nur Hallo. Warte bitte eine Weile auf Antwort, da die User auch ein Privatleben haben ;-) (PS: Ich bin nur ein Bot)"
