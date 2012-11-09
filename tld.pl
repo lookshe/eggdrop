2f1137c606eaa5e599b83a53b5187e6970348852
lookshe <lookshe@fumuga.com>
2012-11-09 20:04:06 +0100

    message if not found a tld

--- tld.pl
+++ tld.pl
@@ -23,6 +23,7 @@ my $scrap = scraper {
 my $url = URI->new("http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains");
 my $res = $scrap->scrape($url);
 my $table = $res->{'table'};
+my $found = 0;
 for ($i = 0; $i < $#$table; $i++)
 {
    if ($$table[$i] =~ /^\.[^ ]*$/ && $$table[$i+1] !~ /^No$/ && $$table[$i+1] !~ /^Yes$/ && $$table[$i+1] !~ /^Partial\[/ && $$table[$i+1] !~ /^$/)
@@ -31,6 +32,7 @@ for ($i = 0; $i < $#$table; $i++)
       if ($$table[$i] =~ /^$type$/)
       {
          print "$type is $$table[$i+1]\n";
+         $found = 1;
          break;
       }
       $i++;
@@ -42,9 +44,14 @@ for ($i = 0; $i < $#$table; $i++)
       if ($$table[$i+1] =~ /^$type$/)
       {
          print "$type is $$table[$i+2]\n";
+         $found = 1;
          break;
       }
       $i+=5;
       next;
    }
+}
+if ($found == 0)
+{
+   print "$type is unknown\n";
 }
